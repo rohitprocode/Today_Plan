0acf4d6043865c2f41d91d0e4290778d20b6249d
rohitprocode <rohit990797@gmail.com>
2023-10-08 20:10:26 +0530

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-// import 'D:\Rohit Coding Resources\React Practice\today-plan\node_modules\bootstrap\dist\css\bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Today's Plan</h1>
+      <div className='container-fluid my-5' >
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-white shadow-lg p-3">
+          <div className="text-center"><h1>Today's Plan</h1></div>
+
+          </div>
+        </div>
       </div>
     )
   }
