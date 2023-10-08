92d05272b9312fbf5483c84a596dde70b0718cc4
rohitprocode <rohit990797@gmail.com>
2023-10-08 22:53:27 +0530

    Add Button

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends Component {
           <h2 className="text-center" >Today's Plan</h2>
           <div className="row">
             <div className="col-9">
-              <input type="text" className='form-control' name="" id="" />
+              <input type="text" className='form-control' name="" id="" placeholder='Write Plan Here' />
+            </div>
+            <div className="col-2">
+              <button className='btn btn-warning px-5 fw-bold' >Add</button>
             </div>
           </div>
           </div>
