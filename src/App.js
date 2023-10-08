5884372eeed92bd9f50ddc0cb4a013d4312a6be4
rohitprocode <rohit990797@gmail.com>
2023-10-08 23:26:43 +0530

    Input field onChange

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import React, { Component } from 'react'
+import Plan from './Plan';
 
 class App extends Component {
+  state ={
+    item : [],
+    text : "" 
+  }
+  handlerClick = e =>{
+    this.setState({
+      text : e.target.value
+    })
+  }
   render() {
     return (
       <div className='container-fluid my-5' >
@@ -12,10 +22,13 @@ class App extends Component {
           <h2 className="text-center" >Today's Plan</h2>
           <div className="row">
             <div className="col-9">
-              <input type="text" className='form-control' name="" id="" placeholder='Write Plan Here' />
+              <input type="text" className='form-control' name="" id="" placeholder='Write Plan Here' value={this.state.text}  onChange={this.handlerClick}  />
             </div>
             <div className="col-2">
               <button className='btn btn-warning px-5 fw-bold' >Add</button>
+            </div>
+            <div className="container-fluid">
+              <ul className="list-unstyled row m-5 "><Plan/></ul>
             </div>
           </div>
           </div>
---
+++ src/Plan.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Plan(props) {
+  return (
+    <div>
+    <li>Plan 1</li>
+    </div>
+  )
+}
+
+export default Plan
